310ad0e35221e53f51367e074d3717b9131fe5d2
SissiCR <sissi_033@hotmail.com>
2021-03-06 23:42:03 +0100

    added all the functions to solve the sudoku

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,15 +1,57 @@
-
+import time
 
 class Sudoku:
     def __init__(self, name):
         self.name = name
         self.grid = [[0 for x in range(9)] for y in range(9)]
-    
+          
     def addRow(self, row, data):
         for column in range(0, 9):
             number =  data[0][column]
-            self.grid[row][column] = number
-       
+            self.grid[row][column] = int(number)
+
+    def getCelltoSolve(self):
+        for x in range(len(self.grid)):
+            for c in range(len(self.grid)):
+                if self.grid[x][c] is 0:
+                    return x, c
+        return False
+
+    def solve(self):     
+        if self.getCelltoSolve() is False:
+            return True
+        row, column = self.getCelltoSolve()
+        self.val = self.getAllowValues(row, column)
+        for x in self.val:
+            self.grid[row][column] = x
+            if self.solve() is True:
+                return True
+            self.grid[row][column]= 0
+        return False
+
+    def checkRow(self,row, value):
+        return all([value != self.grid[row][x] for x in range(9)])
+
+    def checkColumn(self,column, value):
+        return all([value != self.grid[x][column] for x in range(9)])
+
+    def checkMatrix(self,row, column, value):
+         secTopX, secTopY = 3*(row//3), 3*(column//3)
+         for x in range(secTopX, secTopX + 3):
+            for y in range(secTopY, secTopY + 3):
+                if self.grid[x][y] == value:
+                    return False 
+         return True
+                
+    def getAllowValues(self,i, j):
+       values = []
+       for x in range(1, 10):
+          if self.checkRow(i,x): 
+            if self.checkColumn(j,x):
+               if self.checkMatrix(i, j, x):
+                    values.append(x)      
+       return values
+
     def print(self):
         print(self.name)
         print("-"*27)
@@ -24,18 +66,8 @@ class Sudoku:
             print(row)
             if r % 3 == 2:
                print("-"*27)
-        print('\n')
+        print('\n')    
 
-
-
-
-
-      
-
-#solve, backtracking
-#find empty space
-#test if number is approved
-#print sudoku
 
 def main():
     sudokus = []
@@ -59,13 +91,13 @@ def main():
                     j = j + 1
                 
     retrieveItemsFromFile() 
+    t = time.process_time()
     for x in sudokus:
-        x.print()
-
-    #loop the list of sudokus, solve them and print them
-    #capture time
-
-
+      x.solve()
+    elapsedTime = time.process_time() - t
+    print('Time:', elapsedTime)
+    for x in sudokus:
+      x.print()
 
 if __name__=="__main__": 
     main() 
